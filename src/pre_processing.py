de8bddebd47325ae6558c1df47cb1c718d120002
Luke Kiernan <lukekiernan@stonybrook.edu>
2024-10-24 13:34:32 -0500

    other color spaces and metal gpus

--- src/pre_processing.py
+++ src/pre_processing.py
@@ -1,5 +1,6 @@
 import torchvision.transforms as transforms
 import warnings
+from skimage import color
 
 transform_basic = transforms.Compose([
     transforms.ToTensor(),
@@ -31,6 +32,24 @@ transform_Img_grayscale = transforms.Compose([
     transforms.Resize((256,256))
 ])
 
+class ConvertColorTransform(object):
+    def __init__(self, fromspace, tospace):
+        def validate_space(space):
+            if space.lower() not in ['rgb', 'lab', 'gray', 'luv', 'xyz', 'hsv']:
+                warnings.warn(f"colorspace {space} not recognized, defaulting to rgb")
+                return 'rgb'
+            else:
+                return space.lower()
+        fromspace = validate_space(fromspace)
+        tospace = validate_space(tospace)
+        if hasattr(color, f"{fromspace}2{tospace}"):
+            self.transform = getattr(color, f"{fromspace}2{tospace}")
+        else:
+            warnings.warn("Compatible color conversion not found.")
+            self.transform = lambda x:x
+    def __call__(self, arr):
+        return self.transform(arr)
+
 def getTransform_in(CONFIG):
     if "IN_COLORMAP" in CONFIG.keys():
         if CONFIG["IN_COLORMAP"] == "GRAY":
@@ -52,6 +71,13 @@ def getTransform_out(CONFIG):
             return transform_basic
         elif CONFIG["OUT_COLORMAP"] == "RGB_IMG":
             return transform_Img
+        elif CONFIG["OUT_COLORMAP"].lower() in ['lab', 'luv', 'xyz', 'hsv']:
+            outType = CONFIG["OUT_COLORMAP"].lower()
+            return transforms.Compose([transforms.ToPILImage(),
+                                   ConvertColorTransform('rgb', outType),
+                                   transforms.ToTensor(dtype='float32'),
+                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                                    transforms.Resize((32,32))])
         else: 
             warnings.warn("OUT_COLORMAP not recognized. Using default RGB")
             return transform_basic
