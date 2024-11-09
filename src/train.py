ad6433d9d431ea498dadd3ad2fb791dbad640862
Sushruth <sushins94@gmail.com>
2024-11-09 11:35:17 -0500

    Fixes to resume training and save checkpoint in case of crash

--- src/train.py
+++ src/train.py
@@ -8,6 +8,7 @@ from torchsummary import summary
 import gc
 import torch.nn as nn
 import time
+import numpy as np
 
 # Function to plot the loss curve
 def plot_loss_curve(losses_train, losses_val = None):
@@ -15,7 +16,8 @@ def plot_loss_curve(losses_train, losses_val = None):
     plt.figure(figsize=(10, 5))
     plt.plot(losses_train, label='Train')
     if losses_val:
-        plt.plot(losses_val, label='Validation')
+        losses_val = np.array(losses_val)
+        plt.plot(losses_val[:,0],losses_val[:,1], label='Validation')
     plt.title('Loss Curves')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
@@ -104,12 +106,39 @@ def train_model(train_loader,valid_loader,model,optimizer,scheduler,CONFIG,crite
     num_epochs = CONFIG['epochs']
     start = time.time()
     for epoch in range(num_epochs):
+        if CONFIG['resume_training']:
+            if epoch == 0:
+                checkpoint = torch.load(CONFIG['resume_checkpoint'])
+                model.load_state_dict(checkpoint['state_dict'])
+                optimizer.load_state_dict(checkpoint['optimizer'])
+                #scheduler.load_state_dict(checkpoint['scheduler'])
+                for key in checkpoint['CONFIG']:
+                    if key in CONFIG:
+                        CONFIG[key] = checkpoint['CONFIG'][key]
+                #CONFIG = checkpoint['CONFIG']
+                losses_train = checkpoint['train_loss']
+                losses_val = checkpoint['valid_loss']
+                lr_history = checkpoint['lr_rate']
+                
+                epoch_n = len(losses_train)
+                epoch_val_loss = losses_val[-1][1]
+                if scheduler is not None:
+                    scheduler = fetch_scheduler(optimizer, CONFIG)
+                    for _ in range(epoch):
+                        scheduler.step()
+                plot_loss_curve(losses_train, losses_val)
+                
+                print(f"Attempting to resume training from epoch {len(losses_train)}")
+            if epoch < epoch_n:
+                print(f"Skipping epoch {epoch+1} until resuming epoch {epoch_n+1}")
+                continue
+        #Train the model
         epoch_loss = train_one_epoch(model, optimizer, train_loader, epoch,  CONFIG, criterion)
         losses_train.append(epoch_loss)
         #Evaluate the model on validation set
-        if valid_loader:
+        if valid_loader and ((epoch+1)% CONFIG['validate_every_epoch'] == 0 or epoch == 0 ):
             epoch_val_loss = validate_one_epoch(model, valid_loader, epoch, CONFIG, criterion)
-            losses_val.append(epoch_val_loss)
+            losses_val.append([epoch,epoch_val_loss])
         # Step the scheduler
         if scheduler is not None:
             scheduler.step()
@@ -120,7 +149,14 @@ def train_model(train_loader,valid_loader,model,optimizer,scheduler,CONFIG,crite
         # Plot the loss curve
         if (epoch+1)% CONFIG['display_loss_epoch'] == 0 or epoch == num_epochs-1:
             plot_loss_curve(losses_train, losses_val)
+        
         tqdm.write(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {epoch_loss:.4f}, Validation Loss: {epoch_val_loss:.4f}, LR: {lr_rate:.6f}")
+        #Save the model in every save_every_epoch to the disk in case of a crash
+        if (epoch +1)%CONFIG['save_every_epoch'] == 0 or epoch == num_epochs-1:
+            checkpoint = {'model': model,'state_dict': model.state_dict(),'optimizer': optimizer.state_dict(),#'scheduler': scheduler.state_dict(),
+                'CONFIG': CONFIG,'train_loss': losses_train,'valid_loss': losses_val,'lr_rate': lr_rate}
+            now = time.localtime()
+            torch.save(checkpoint, f"../models/tmp/{CONFIG['model']}_{time.strftime('%Y-%m-%d_%H_%M', now)}_ep{epoch+1}.pth")
     end = time.time()
     time_elapsed = end - start
     print('Training complete in {:.0f}h {:.0f}m {:.0f}s'.format(
